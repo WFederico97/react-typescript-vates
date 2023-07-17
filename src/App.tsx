851d3bc85215c05d785fc39db4f66648ed695a89
Federico W <wuthrich09@gmail.com>
2023-07-17 12:20:57 -0300

    Minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Todos from './components/Todos'
+import Todo from './models/todo'
 import './App.css'
 
 function App() {
-  
+
+  const todos = [
+    new Todo('Buy a phone'),
+    new Todo('Learn Typescript'),
+    new Todo('Get paid'),
+    new Todo('Flirt'),
+    new Todo('Shop Groceries'),
+  ];
+
   return (
     <>
       <div>
-        <Todos/>
+        <Todos items={todos}/>
       </div>
     </>
   )
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
-const Todos = () => {
+import React from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  // ** React.FC es un functional component
   return (
-    <div>Todos</div>
-  )
-}
+    <>
+      <ul>
+        {props.items.map((item) => (
+          <TodoItem key={item.id} text={item.text} />
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default Todos
+export default Todos;
